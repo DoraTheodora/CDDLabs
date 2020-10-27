76d37625d90d14032922fb42f38c14212364be5f
Theodora <theodora@md>
2020-10-27 09:51:05 +0000

    Lab2 Commit

---
+++ Lab2_Semaphores/signal.cpp
@@ -0,0 +1,40 @@
+#include "Semaphore.h"
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+/*! \class Signal
+    \brief An Implementation of Threads Using Semaphores 
+
+   Uses C++11 features such as mutex and condition variables to implement Semaphores in thread functions 
+
+*/
+/*! displays a message first*/
+void taskOne(std::shared_ptr<Semaphore> theSemaphore, int delay){
+  sleep(delay);
+  std::cout <<"I ";
+  std::cout << "must ";
+  std::cout << "print ";
+  std::cout << "first"<<std::endl;
+}
+/*! displays a message second*/
+void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
+  std::cout <<"This ";
+  std::cout << "will ";
+  std::cout << "appear ";
+  std::cout << "second"<<std::endl;
+}
+
+
+int main(void){
+  std::thread threadOne, threadTwo;
+  std::shared_ptr<Semaphore> sem( new Semaphore);
+  /**< Launch the threads  */
+  int taskOneDelay=5;
+  threadOne=std::thread(taskTwo,sem);
+  threadTwo=std::thread(taskOne,sem,taskOneDelay);
+  std::cout << "Launched from the main\n";
+   /**< Wait for the threads to finish */
+  threadOne.join();
+  threadTwo.join();
+  return 0;
+}
