55541358e646286c6598f6baf0d75e38185520fd
Theodora <theodora@md>
2020-11-03 10:28:01 +0000

    Lab2 - main.cpp done

--- Lab3_Rendezvous/main.cpp
+++ Lab3_Rendezvous/main.cpp
@@ -18,34 +18,34 @@
 
 */
   /*! 
-  \fn void taskOne(std::shared_ptr<Semaphore> firstSem,std::shared_ptr<Semaphore>  secondSem, int delay)
+  \fn void taskOne(std::shared_ptr<Semaphore> taskOneArrived,std::shared_ptr<Semaphore>  taskTwoArrived, int delay)
   \brief This is a thread that sleeps for a certain amount of seconds, and then prints to the screen "I must print first"
-  \param firstSem gives the function the control of the first part of the function
-  \param secondSem gives the function the control when for the second part of the function
+  \param taskOneArrived gives the function the control of the first part of the function
+  \param taskTwoArrived gives the function the control when for the second part of the function
   \param delay indicates the time the thread will sleep
   displays a message that is split in to 2 sections to show how a rendezvous works*/
-void taskOne(std::shared_ptr<Semaphore> firstSem,std::shared_ptr<Semaphore>  secondSem, int delay){
+void taskOne(std::shared_ptr<Semaphore> taskOneArrived,std::shared_ptr<Semaphore>  taskTwoArrived, int delay){
   std::this_thread::sleep_for(std::chrono::seconds(delay));
-  firstSem->Signal();
   std::cout << "Task One has arrived! "<< std::endl;
+  taskOneArrived->Signal();
   //THIS IS THE RENDEZVOUS POINT!
-  secondSem->Signal();
+  taskTwoArrived->Wait();
   std::cout << "Task One has left! "<<std::endl;
 }
 
   /*! 
   \fn void taskTwo(std::shared_ptr<Semaphore> firstSem, std::shared_ptr<Semaphore> secondSem, int delay)
   \brief This runs divided into 2, the first statement will print and then will give control to the next function.
-  \param firstSem puts the thread in the queue waiting for its turn
-  \param secondSem puts the second part of the thread in the waiting queue, waiting for its turn 
+  \param taskOneArrived puts the thread in the queue waiting for its turn
+  \param taskTwoArrived puts the second part of the thread in the waiting queue, waiting for its turn 
   \param delay indicates the time the thread will sleep
   displays a message that is split in to 2 sections to show how a rendezvous works*/
-void taskTwo(std::shared_ptr<Semaphore> firstSem, std::shared_ptr<Semaphore> secondSem, int delay){
+void taskTwo(std::shared_ptr<Semaphore> taskOneArrived, std::shared_ptr<Semaphore> taskTwoArrived, int delay){
   std::this_thread::sleep_for(std::chrono::seconds(delay));
-  firstSem->Wait();
   std::cout << "Task Two has arrived! "<<std::endl;
+  taskTwoArrived->Signal();
   //THIS IS THE RENDEZVOUS POINT!
-  secondSem->Wait();
+  taskOneArrived->Wait();
   std::cout << "Task Two has left! "<<std::endl;
 }
 
@@ -58,7 +58,7 @@ int main(void){
   std::thread threadOne, threadTwo;
   std::shared_ptr<Semaphore> sem1( new Semaphore);
   std::shared_ptr<Semaphore> sem2( new Semaphore);
-  int taskOneDelay=5;
+  int taskOneDelay=1;
   int taskTwoDelay=1;
   /**< Launch the threads  */
   threadOne=std::thread(taskTwo,sem1,sem2,taskTwoDelay);
